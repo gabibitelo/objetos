5ae8f551542b8cc57cc6cd9d22427b6dd95746b7
emily <biteloemily@gmail.com>
2023-04-28 21:38:17 -0300

    exercitando programação

---
+++ lindex.js
@@ -0,0 +1,64 @@
+// 28 do 04
+
+//testes 
+
+//const professor = {
+  //  nome: 'senac' ,
+    //idade: 28,
+    //email: 'senac@senac.com'
+
+//}
+//console.log(professor.nome, professor.idade)
+//console.log('email: ' + professor.email)
+
+//const bicicleta = {
+  //  cor: 'azul',
+    //marchas: 21,
+    //estilo: 'espeed',
+//}
+
+//console.log(bicicleta["estilo"], bicicleta["marchas"])
+
+
+//altetrar valores
+
+//bicicleta.marchas = 18;
+//console.log(bicicleta.marchas)
+
+
+//exercicio 1
+
+//crie um objeto que represente um filme. Ela deve ter dados da direção, o nome, o ano de lançamento,
+//e uma lista que diga ce voçe ja viu ou não.
+
+//const filme = {
+  //empresa: 'disney' ,
+  //dadosDaDirecao: 'cristina 29 anos , carolina 23 , bruno 45 ,' ,
+  //nome: 'gatoDeBotas' ,
+  //anoDeLançamento: '2007' ,
+  //lista: 'já vi' ,
+
+
+
+//}
+//console.log(filme.empresa ,)
+//console.log(filme.dadosDaDirecao ,)
+//console.log(filme.nome ,)
+//console.log(filme.anoDeLançamento ,)
+//console.log(filme.lista ,)
+
+//exercicio 2
+
+
+//A) crie um objeto que represente uma pessoa, Essa pessoa precisa ter nome, idade, genero musical preferido;
+
+//B)acesse e imprima no console as propriedades desse objeto.
+
+
+const pessoa = {
+  nome: 'gabi' ,
+  idade: '16' ,
+  genero: 'pop'
+
+}
+console.log("o nome da pessoa é" , pessoa['nome'] , "ela tem" , pessoa['idade'] , "seu genero musical é" , pessoa['genero'])
