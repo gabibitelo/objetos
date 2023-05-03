95ae4e150c6bcbdd05ff9016082792d3588059d6
emily <biteloemily@gmail.com>
2023-05-02 22:02:54 -0300

    exercicios

--- lindex.js
+++ lindex.js
@@ -45,7 +45,7 @@
 //console.log(filme.dadosDaDirecao ,)
 //console.log(filme.nome ,)
 //console.log(filme.anoDeLançamento ,)
-//console.log(filme.lista ,)
+//console.log(filme.lista
 
 //exercicio 2
 
@@ -55,10 +55,111 @@
 //B)acesse e imprima no console as propriedades desse objeto.
 
 
-const pessoa = {
-  nome: 'gabi' ,
-  idade: '16' ,
-  genero: 'pop'
+//const pessoa = {
+  //nome: 'gabi' ,
+  //idade: '16' ,
+  //genero: 'pop'
+
+//}
+//console.log("o nome da pessoa é" , pessoa['nome'] , "ela tem" , pessoa['idade'] , "seu genero musical é" , pessoa['genero'])
+
+
+//continuação objetos
+//02/05
+
+//ARRAY DE OBJETOS
+
+//const professores = [
+ // {nome: 'andrei' , modulo: 1} ,
+ // {nome: 'vitor' , modulo: 2} ,
+ // {nome: 'mina' , modulo: 3} ,
+
+//]
+
+//(teste)console.log(professores)
+
+//(teste)console.log(professores[0])
+
+//(verdadeiro) = console.log(professores[0].nome)//possição do array em que o objeto se encontra.
+
+//const curso = {
+  //nome: 'frontend' //STRING
+  
+//}
+
+//adicionando atributos
+
+//curso.numeroEstudante = 50;
+
+//console.log(curso.nome)  //frontend
+//console.log(curso.numeroEstudante)
+
+//const Novofilme = {
+  //personagens: [ 'os daumatas' , 'cruela' , 'cachorros' ],
+  //empresa: 'disney' ,
+  //nome: 'cruela' ,
+  //anoDeLançamento: '2010' , 
+  //lista: 'ss' ,
+  //elenco: [ 'maria' , 'joao' , 'vitor'],  
+//}
+
+//Novofilme.personagens = [ 'os daumatas' , 'cruela' , 'cachorros'  ];
+//console.log (Novofilme.elenco[0], "como" ,Novofilme.personagens[0] );
+//console.log (Novofilme.elenco[1], "como" ,Novofilme.personagens[1] );
+//console.log (Novofilme.elenco[2], "como" ,Novofilme.personagens[2] );
+//Novofilme.elenco[0] = 'xuxa';
+//console.log(Novofilme.elenco[0] = 'xuxa');
+
+//arrays e oobjetos
+
+//const usario = {
+  //nome: 'emily' ,
+  //idade: 16,
+  //email: '@emilygabi' ,
+  //cidade: 'são léo',
+  //marcas: ['dell' , 'lg' , 'nokia',]
+//} 
+
+//troca de nome
+
+ //const usarionovo = {
+  //...usario ,
+  //nome: 'gabi'
+
+ //}
+ //console.log(usarionovo)
+
+
+ //exercicio-4)
+ //crie uma função que receba um objeto de pessoa (exercicio 2) e crie um novo
+ //objeto mantendo as propriedades originais e acrescentando:
+
+ //A)uma propriedade com a lista de suas comidas favoritas
+ //B)uma propriedade que seja um objeto , com o nome e idade para
+ //representar o melhor amigo da pessoa 
+
+ const usarioTeste = {
+  nome: 'emily' ,
+  idade: 16,
+  email: '@emilygabi' ,
+  cidade: 'são léo',
+ }
+
+novoUsario(usarioTeste)
+function novoUsario(pessoa) {
+  const pessoaNova = {
+    ...pessoa ,s
+  }
+
 
 }
-console.log("o nome da pessoa é" , pessoa['nome'] , "ela tem" , pessoa['idade'] , "seu genero musical é" , pessoa['genero'])
+
+
+ const novapessoa = {
+
+  comidaFav: ['arroz' , 'feijao' , 'ovo' , 'batataDoce'],
+  amigo: {nome: 'maria' , idade: '16'},
+ }
+ console.log(`o nome da pessoa é ${teste.nome} e suas comidas favs sao ${teste.comidaFav[0]} e ${teste.comidaFav[1]} `)
+
+ 
